7fbd2d770976344f3f6ae5c3d77312bdfa5dbafc
Jace Roldan <jaceroldan16@gmail.com>
2024-06-25 00:40:34 +0800

    Create initial Todo endpoint

---
+++ todo/api_sileo.py
@@ -0,0 +1,18 @@
+from sileo.resource import Resource
+from sileo.registration import register
+
+from .models import Todo
+from .forms import TodoForm
+
+
+class TodoResource(Resource):
+
+    query_set = Todo.objects.filter(removed=False)
+    fields = ('label', 'pk', 'done', 'todo_uuid')
+    allowed_methods = ('filter','get_pk', 'create', 'delete','update')
+    form_class = TodoForm
+    delete_filter_fields = ['pk']
+    update_filter_fields = ('pk',)
+    
+
+register(namespace="todo", name="todo", resource=TodoResource)
