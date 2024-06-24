4e00fcb88923313a7c4776cd633c1899e7ba65a6
Jace Roldan <jaceroldan16@gmail.com>
2024-06-25 00:36:18 +0800

    Add removed field

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,4 @@ class Todo(models.Model):
     todo_uuid = models.CharField(max_length=255)
     done = models.BooleanField(default=False)
     label = models.CharField(max_length=100)
+    removed = models.BooleanField(default=False)
